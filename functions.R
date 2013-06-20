07689d8494a244e113676e8761f0a2b277c32fc1
François <briatte@gmail.com>
2013-06-20 15:25:33 +0200

    fixing df bug (thanks Yihui)

--- functions.R
+++ functions.R
@@ -10,53 +10,53 @@ by.group <- function(f) {
 #
 # who is followed by...
 #
-who.is.followed.by <- function(df, x) {
-  f = df[which(df$Source == x), ]$Target
+who.is.followed.by <- function(net, x) {
+  f = net[which(net$Source == x), ]$Target
   by.group(f)
   return(list(node = x, follows = as.character(f)))
 }
 # examples
-# x = who.is.followed.by(df, "JacquesBompard")
+# x = who.is.followed.by(net, "JacquesBompard")
 # str(x)
-# y = who.is.followed.by(df, "Marion_M_Le_Pen")
+# y = who.is.followed.by(net, "Marion_M_Le_Pen")
 # str(y)
 
 #
 # who follows...
 #
-who.follows <- function(df, x) {
-  f = df[which(df$Target == x), ]$Source
+who.follows <- function(net, x) {
+  f = net[which(net$Target == x), ]$Source
   by.group(f)
   return(list(node = x, followers = as.character(f)))
 }
 # examples
-# x = who.follows(df, "nk_m")
+# x = who.follows(net, "nk_m")
 # str(x)
-# y = who.follows(df, "marclefur")
+# y = who.follows(net, "marclefur")
 # str(y)
 
 #
 # within-group indegree
 #
-top.group.inlinks <- function(df, x) {
+top.group.inlinks <- function(net, x) {
   x = ids$Twitter[which(ids$Groupe %in% x)]
-  f = subset(df, Source %in% x & Target %in% x)
+  f = subset(net, Source %in% x & Target %in% x)
   f = data.frame(table(f$Target))
   return(head(f[order(f$Freq, decreasing = TRUE), ]))
 }
 # examples
-# top.group.inlinks(df, "SRC")
+# top.group.inlinks(net, "SRC")
 # lapply(levels(ids$Groupe), top.group.inlinks, df = df)
 
 #
 # within-group outdegree
 #
-top.group.outlinks <- function(df, x) {
+top.group.outlinks <- function(net, x) {
   x = ids$Twitter[which(ids$Groupe %in% x)]
-  f = subset(df, Source %in% x & Target %in% x)
+  f = subset(net, Source %in% x & Target %in% x)
   f = data.frame(table(f$Source))
   return(head(f[order(f$Freq, decreasing = TRUE), ]))
 }
 # examples
-# top.group.outlinks(df, "Ecolo")
+# top.group.outlinks(net, "Ecolo")
 # lapply(levels(ids$Groupe), top.group.outlinks, df = df)
